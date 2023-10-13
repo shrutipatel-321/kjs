badacb98ef10fe961f6f0a1d914a122402b4c92d
Ankit Madhukar <ankitmadhukar23@gmail.com>
2023-10-13 19:14:56 +0530

    .

--- src/Components/LandingPage/Landing.js
+++ src/Components/LandingPage/Landing.js
@@ -9,16 +9,16 @@ import './Landing.css';
 function Landing() {
   return (
     <div className="landing">
-      <video className="v1" autoPlay loop muted>
+      <video className="v1" id="vid1"  autoPlay loop muted>
         <source src={video1} type="video/mp4" />
       </video>
-      <video className="v1" autoPlay loop muted>
+      <video className="v1" id="vid2" autoPlay loop muted>
         <source src={video2} type="video/mp4" />
       </video>
-      <video className="v1" autoPlay loop muted>
+      <video className="v1" id="vid3" autoPlay loop muted>
         <source src={video3} type="video/mp4" />
       </video>
-      <video className="v1" autoPlay loop muted>
+      <video className="v1" id="vid4" autoPlay loop muted>
         <source src={video4} type="video/mp4" />
       </video>
       
