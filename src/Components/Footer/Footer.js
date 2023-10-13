46b1ec73e0b418d5096d3c6d14160913e35cc2c0
shrutipatel-321 <shrutipatelnakotana177026@gmail.com>
2023-10-13 23:44:49 +0530

    .

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
-function Footer(){
-    return (
-    
-        <div className="footer-1">
-            <h1 id='footer-h1'>Contact Form</h1>
-            
-
-
+function Footer() {
+  return (
+    <div className="footer-1">
+      <h1 id="footer-h1">Contact Form</h1>
+      <form id="contact-form">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" />
         </div>
-    )
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea className="form-control" rows="5"></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Footer;
