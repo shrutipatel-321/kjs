567f5fb08a274e8708b1b57b6beadc945d9c15bd
Ankit Madhukar <ankitmadhukar23@gmail.com>
2023-10-14 15:20:39 +0530

    .

--- src/Components/LandingPage/Landing.js
+++ src/Components/LandingPage/Landing.js
@@ -5,11 +5,25 @@ import video1 from "./v1.mp4";
 import video2 from "./v2.mp4";
 import video3 from "./v3.mp4";
 import video4 from "./v4.mp4";
+// import img1 from './s1.png';
+import img2 from './s21.png';
+import img3 from './s22.png';
+import img4 from './s23.png';
 import "./Landing.css";
 
 function Landing() {
   return (
     <>
+    {/* <img className="strawberry" src={img1} alt="" /> */}
+    <img className="strawberry" id="s1" src={img2} alt="" />
+    <img className="strawberryy" id="s2" src={img3} alt="" />
+    <img className="strawberry" id="s3" src={img4} alt="" />
+    <img className="strawberryy" id="s4" src={img2} alt="" />
+    <img className="strawberry" id="s5" src={img3} alt="" />
+    <img className="strawberry" id="s6" src={img4} alt="" />
+    <img className="strawberryy" id="s7" src={img2} alt="" />
+    <img className="strawberry" id="s8" src={img3} alt="" />
+    <img className="strawberryy" id="s9" src={img4} alt="" />
     <div className="container">
       <div className="landing">
         <video className="v1" id="vid1" autoPlay loop muted>
