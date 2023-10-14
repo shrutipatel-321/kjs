58b6d8223712de4578028cc862db42f7c04d1b80
Ankit Madhukar <ankitmadhukar23@gmail.com>
2023-10-14 20:27:34 +0530

    .

--- src/Components/LandingPage/Landing.js
+++ src/Components/LandingPage/Landing.js
@@ -5,13 +5,38 @@ import video1 from "./v1.mp4";
 import video2 from "./v2.mp4";
 import video3 from "./v3.mp4";
 import video4 from "./v4.mp4";
+// import img1 from './s1.png';
+import img2 from './candy.png';
+import img3 from './candy.png';
+import img4 from './candy.png';
 import "./Landing.css";
 
 function Landing() {
   return (
     <>
+    {/* <img className="strawberry" src={img1} alt="" /> */}
     <div className="container">
+    
+    <img className="strawberryy" id="s2" src={img3} alt="" />
+    <img className="strawberryy" id="s9" src={img4} alt="" />
+    <img className="strawberryy" id="s4" src={img2} alt="" />
+    <img className="strawberry" id="s6" src={img4} alt="" />
+    <img className="strawberryy" id="s8" src={img3} alt="" />
       <div className="landing">
+      
+      <img className="strawberryy" id="s1" src={img2} alt="" />
+    <img className="strawberry" id="s3" src={img4} alt="" />
+    
+    <img className="strawberry" id="s5" src={img3} alt="" />
+    
+    <img className="strawberryy" id="s7" src={img2} alt="" />
+    <img className="strawberry" id="s10" src={img4} alt="" />
+    <img className="strawberry" id="s11" src={img3} alt="" />
+    <img className="strawberry" id="s12" src={img2} alt="" />
+    <img className="strawberryy" id="s13" src={img3} alt="" />
+    <img className="strawberry" id="s14" src={img4} alt="" />
+    
+    
         <video className="v1" id="vid1" autoPlay loop muted>
           <source src={video1} type="video/mp4" />
         </video>
